f52645c2e1a64299e0d235a9d360d964e015941a
Nishant garg <147591771+NishantGarg123@users.noreply.github.com>
2024-08-05 07:14:58 +0000

    completing the CRUD operation by connecting the mongoDB with nodejs

--- 7-mongo/index.js
+++ 7-mongo/index.js
@@ -1,192 +1,114 @@
 //======================================================================================>>
-//creating the connection with our mongodb.                
-  const mongoose = require('mongoose');
+//creating the connection with our mongodb. 
+const mongoose =require('mongoose');
 
-  main()
-  .then((res)=>{
-          console.log("connection successfull")  ;
-      })
-  .catch(err => console.log(err));
+main()
+.then(res=>{ console.log("connection successful");})
+.catch(err=>{ console.log(err);});
 
-  async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  }
-
-//======================================================================================>>
-
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+//======================================================================================>>]
 
 //======================================================================================>>
 //(schema)creating schema of our documents that will follow by collection
 
-  const userschema = new mongoose.Schema({
-
-      name:String,
-      email:String,
-      age:Number
-  });
+    const bookSchema = new mongoose.Schema({
+        title:{
+            type:String,
+            required:true       //require true define that now it can not we null(similar to the sql not noll)
+        },
+        author:{
+            type:String
+        },
+        price:{
+            type:Number,
+            min:[1,"price is too low for amazon products"]      //2nd message in array will display in error when condition not satisfy
+        },
+        discount:{
+            type:Number,
+            default:0
+        },
+        // category:{
+        //     type:String,
+        //     enum: ["fiction" , "non-fiction"]           //enum define that we can enter the value that are in the given arry.
+        // }
+        genre:[String]
+    });
 //======================================================================================>>
 
 //======================================================================================>>
-//(model)defining the model OR (defining the collection name with the schema)
-
-  const User = mongoose.model("User",userschema);
-
-//======================================================================================>>
-
-
-//======================================================================================>>
-//(insert one)inserting the document with in the collection
-
-    // const user1 = new User({name:"Nishant garg", email:"gargacsnishant@gmail.com",age:20})
-    // const user2 = new User({name:"parash garg", email:"parashacs@gmail.com",age:15})
-    // user1.save()
-    // .then((result)=>{console.log(result);})
-    // .catch((err)=>{console.log(err)});
-    // user2.save()
-    // .then((result)=>{console.log(result);})
-    // .catch((err)=>{console.log(err)});
-
-//======================================================================================>>
-
-
-//======================================================================================>>
-//(insert many) inserting multiple users at once
-    // User.insertMany(
-    // [
-    //         {name:"parash" , email:"abc@gmail.com",age:15},
-    //         {name:"shiv", email:"xyz@yahoo.in",age:20}
-    // ]
-    // )
-    // .then((res)=>{console.log(res)})
-    // .catch((err)=>{console.log(err)});
-
+//(modeling)
+const Book = mongoose.model("Book",bookSchema);
 //======================================================================================>>
 
 
 //======================================================================================>>
-//(find)find method return the query that is the thenable query.
+//(insert)
+    //1.
+        // const Book1 = new Book({
+        //     title:"mathmatics XII",
+        //     author:"RD Sharma",
+        //     price:1200
+        // });
 
-    // User.find({age:{$gte:15}})
-    // .then(result=>{
-    //         console.log(result[0].name);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
+        // Book1.save()
+        // .then(res=>{   console.log(res); })
+        // .catch(err=>{  console.log(err); });
 
-    //         User.findOne({age:{$gt:15}})
-    //         .then(result=>{
-    //                 console.log(result);
-    //         })
-    //         .catch(err=>{
-    //                 console.log(err);
-    //         });
+   //2.this code will give error because we have require into the title field.
+        // const Book1 = new Book({
+        //     author:"RD Sharma",
+        //     price:1200
+        // });
 
-    // User.findById("66af8f34ea307759321a8874")
-    // .then(result=>{
-    //         console.log(result);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
+        // Book1.save()
+        // .then(res=>{   console.log(res); })
+        // .catch(err=>{  console.log(err); });
 
-    // User.findByIdAndDelete("66af8f34ea307759321a8874")
-    // .then(result=>{
-    //         console.log(result);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
-//======================================================================================>>
+    //3. 
+        // const Book1 = new Book({
+        //     title:"mathmatics VII",
+        //     price:1200
+        // });
 
-//======================================================================================>>
-//(update)
+        // Book1.save()
+        // .then(res=>{   console.log(res); })
+        // .catch(err=>{  console.log(err); });
 
-    // User.updateOne({name:"shiv"},{age:40})
-    // .then(result=>{
-    //         console.log(result);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
+    //4.Give error due to the constraints of price type tring
+        // const Book1 = new Book({
+        //     author:"nishant",
+        //     title:"generation based ai tols uses",
+        //     price:"abc"
+        // });
 
-    // User.updateMany({name:"shiv"},{age:100})
-    // .then(result=>{
-    //         console.log(result);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
+        // Book1.save()
+        // .then(res=>{   console.log(res); })
+        // .catch(err=>{  console.log(err); });
 
-    // User.find().then(res=>{
-    //         console.log(res);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
+    //5.it is not give error because we have passed a string but it is parseable to the number.
+        // const Book1 = new Book({
+        //     author:"mavel v3",
+        //     title:"cartoons network s3",
+        //     price:"2",
+        //     // category:"comics"
+        //     genre:["comics" ,"superheroes" ,"fiction"]
+        // });
 
-//======================================================================================>>
+        // Book1.save()
+        // .then(res=>{   console.log(res); })
+        // .catch(err=>{  console.log(err); });
 
-//======================================================================================>>
-//update is give the info that how much modified but does not give the  updated document thats why we use update with find
-//(find and update)
-    // User.findOneAndUpdate({name:"parash"},{name:"parash agrawal"},{new:true})     //here true used so that it return the updated value rather then the old value
-    // .then(result=>{
-    //         console.log(result);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
 
-    // User.findByIdAndUpdate("66b053e0565f5f95479386ac",{name:"krishna-chaturvedi"},{new:true})        //here true used so that it return the updated value rather then the old value
-    // .then(result=>{
-    //         console.log(result);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
-      
-//======================================================================================>>
-
-//======================================================================================>>
-//(delete)
-
-    // User.deleteOne({name:"krishna-chaturvedi"})
-    // .then(res=>{
-    //         console.log(res);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
-
-    // User.deleteMany({age:15})
-    // .then(res=>{
-    //         console.log(res);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
 //======================================================================================>>
 
 
 //======================================================================================>>
-//deleteis give the info that how much modified but does not give the  deleted document thats why we use delete with find
-//(find and delete)
+//update and follow the constraints of the schema
+        Book.findByIdAndUpdate("66b078194e14b3a5c27fce06",{price:-400},{runValidators:true},{new:true})     //2nd argument is define that if we update the value then also our constraints(validators) apply.
+        .then(res=>{ console.log(res);})
+        .catch(err=>{ console.log(err.errors.price.properties.message);});
 
-    // User.findOneAndDelete({name:"parash garg"})
-    // .then(res=>{
-    //         console.log(res);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
-
-    // User.findByIdAndDelete("66b064a99fd09d60c8f575c6",{name:"parash garg"})
-    // .then(res=>{
-    //         console.log(res);
-    // })
-    // .catch(err=>{
-    //         console.log(err);
-    // });
-      
 //======================================================================================>>
