520c2bb78f23802d2ee31db561a1e7e8be3dcad6
Nishant garg <147591771+NishantGarg123@users.noreply.github.com>
2024-08-30 07:45:10 +0000

    modification in listing

--- 9-majorProject/models/listing.js
+++ 9-majorProject/models/listing.js
@@ -10,11 +10,11 @@ const listingSchema = new Schema({
     description:String,
     image:{
       type:String,
-      set:(v)=> v === ""?"krishna.png" : v,
-      default:"krishna.png"
+      set:(v)=> v === "" ? "https://images.unsplash.com/photo-1602343168117-bb8ffe3e2e9f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dmlsbGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60" : v,
+      default:"https://images.unsplash.com/photo-1602343168117-bb8ffe3e2e9f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dmlsbGF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
     },
     price:Number,
-    lacation:String,
+    location:String,
     country:String
 });
 
